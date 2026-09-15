df1780d92860d10aecc6ebea429f7faca58f2419
rachelebossazerocarta-cloud <rachelebossazerocarta@gmail.com>
2026-09-15 18:35:48 +0200

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  // Lasciamo il fetch vuoto in modo che i telefoni lo contino come PWA valida
+});
